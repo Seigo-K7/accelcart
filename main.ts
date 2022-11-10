b1c7a27439358a8c310c10981e467aceae19a5ed
Seigo-K7 <117812600+Seigo-K7@users.noreply.github.com>
2022-11-10 15:48:14 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,24 +1,49 @@
-bluetooth.onBluetoothConnected(function () {
-    bluetooth.startAccelerometerService()
-    basic.showIcon(IconNames.Happy)
-    if (input.buttonIsPressed(Button.B)) {
-        basic.pause(1000)
-        trig = control.millis()
-        basic.showIcon(IconNames.Diamond)
-        while (control.millis() - trig < 4000) {
-            bluetooth.uartWriteValue("x", 0)
-        }
-    } else if (input.buttonIsPressed(Button.AB)) {
+radio.setGroup(27)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.AB)) {
+        let trig = 0
         basic.showLeds(`
-            # # # . .
-            # . . # .
-            # . # # .
-            # # . # .
-            # . . . #
+            . . # . .
+            . . # . .
+            # # . # #
+            . . # . .
+            . . # . .
+            `)
+        basic.pause(2000)
+        basic.showLeds(`
+            # . # . #
+            . . # . .
+            # # # # #
+            . . # . .
+            # . # . #
+            `)
+        while (control.millis() - trig < 4000) {
+            radio.sendValue("x", Math.map(input.acceleration(Dimension.X), -1024, 1024, -980, 980) / 100)
+        }
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
             `)
     } else {
-    	
+        if (0 < input.acceleration(Dimension.X)) {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+        } else {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+        }
     }
 })
-let trig = 0
-bluetooth.startUartService()
