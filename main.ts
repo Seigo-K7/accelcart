14dc7eb2436025f103771f437ffa1aa7494d15f2
Seigo-K7 <117812600+Seigo-K7@users.noreply.github.com>
2022-11-10 15:37:39 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
 bluetooth.onBluetoothConnected(function () {
-    while (true) {
-        bluetooth.uartWriteString("HEllo")
+    bluetooth.startAccelerometerService()
+    basic.showIcon(IconNames.Happy)
+    if (input.buttonIsPressed(Button.B)) {
+        basic.pause(1000)
+        trig = control.millis()
+        basic.showIcon(IconNames.Diamond)
+        while (control.millis() - trig < 4000) {
+            bluetooth.uartWriteValue("x", 0)
+        }
+    } else if (input.buttonIsPressed(Button.AB)) {
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # . # # .
+            # # . # .
+            # . . . #
+            `)
+    } else {
+    	
     }
 })
+let trig = 0
 bluetooth.startUartService()
